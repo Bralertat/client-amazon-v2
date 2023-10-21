b4d97f033d397623d05fa3428f6fdcf4306b3d7c
George Default GITHUB Profile <git4code@gmail.com>
2023-10-22 01:33:53 +0400

    cart animation

--- src/components/UI/layout/header/cart/HeaderCart.tsx
+++ src/components/UI/layout/header/cart/HeaderCart.tsx
@@ -2,6 +2,7 @@ import Button from '@/components/UI/button/Button'
 import SquareButton from '@/components/UI/button/SquareButton'
 import { useActions } from '@/hooks/useActions'
 import { useCart } from '@/hooks/useCart'
+import { useDelayUnmount } from '@/hooks/useDelayUnmount'
 import { useOutside } from '@/hooks/useOutside'
 import { OrderService } from '@/services/order.service'
 import { convertPrice } from '@/utils/convertPrice'
@@ -12,6 +13,7 @@ import { FC } from 'react'
 import { RiShoppingCartLine } from 'react-icons/ri'
 import styles from './Cart.module.scss'
 import CartItem from './cart-item/CartItem'
+
 
 const Cart: FC = () => {
   const { isShow, setIsShow, ref } = useOutside(false)
@@ -32,10 +34,13 @@ const Cart: FC = () => {
     {
       onSuccess({ data }) {
         //нправильно тут чистить корзину
-        push(data.confirmation.confirmation_url).then(()=> reset())
+        push(data.confirmation.confirmation_url).then(() => reset())
       }
     }
   )
+  //размонтировать нужно раньше чем закончится анимация 
+  //так как в конце анимации она возвращается на исходную
+  const showDiv = useDelayUnmount(isShow, 250)
 
   return (
     <div className='relative' ref={ref}>
@@ -44,36 +49,38 @@ const Cart: FC = () => {
         onClick={() => setIsShow(!isShow)}
         number={items.length}
       />
-      <div
-        className={cn(
-          `absolute top-[4.2rem] w-80 -left-[12.5rem] bg-secondary 
+      {showDiv && (
+        <div
+          className={cn(
+            `absolute top-[4.2rem] w-80 -left-[12.5rem] bg-secondary 
           rounded-xl px-5 py-3 text-sm z-20 text-white`,
-          isShow ? 'open-menu' : 'close-menu', console.log('cartt-----') as any
-        )}
-      >
-        <div className='font-normal mb-5 text-lg'>My cart</div>
-        <div className={styles.cart}>
-          {items.length ? (
-            items.map(item => <CartItem item={item} key={item.id} />)
-          ) : (
-            <div className='font-light'>Cart is empty!</div>
+            isShow ? 'open-menu' : 'close-menu'
           )}
+        >
+          <div className='font-normal mb-5 text-lg'>My cart</div>
+          <div className={styles.cart}>
+            {items.length ? (
+              items.map(item => <CartItem item={item} key={item.id} />)
+            ) : (
+              <div className='font-light'>Cart is empty!</div>
+            )}
+          </div>
+          <div className={styles.footer}>
+            <div>Total:</div>
+            <div>{convertPrice(total)}</div>
+          </div>
+          <div className='text-center'>
+            <Button
+              onClick={() => mutate()}
+              variant='white'
+              size='sm'
+              className='mt-5 mb-2 btn-link???'
+            >
+              Place order
+            </Button>
+          </div>
         </div>
-        <div className={styles.footer}>
-          <div>Total:</div>
-          <div>{convertPrice(total)}</div>
-        </div>
-        <div className='text-center'>
-          <Button
-            onClick={() => mutate()}
-            variant='white'
-            size='sm'
-            className='mt-5 mb-2 btn-link???'
-          >
-            Place order
-          </Button>
-        </div>
-      </div>
+      )}
     </div>
   )
 }
